aa72fc3bb697e9e408d1eb0da0189561fb744c23
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-04-04 20:45:15 -0300

    little refactor

--- main.go
+++ main.go
@@ -34,32 +34,49 @@ func checkDomain(domain string) {
 
 	hasMX = len(mxRecords) > 0
 
-	txtRecords, err := net.LookupTXT(domain)
+	getRecords(domain, "v=spf1", &hasSPF, &spfRecord)
+	// txtRecords, err := net.LookupTXT(domain)
 
-	if err != nil {
-		log.Printf("Error: %v\n", err)
-	}
+	// if err != nil {
+	// 	log.Printf("Error: %v\n", err)
+	// }
 
-	for _, record := range txtRecords {
-		if strings.HasPrefix(record, "v=spf1") {
-			hasSPF = true
-			spfRecord = record
-			break
-		}
-	}
+	// for _, record := range txtRecords {
+	// 	if strings.HasPrefix(record, "v=spf1") {
+	// 		hasSPF = true
+	// 		spfRecord = record
+	// 		break
+	// 	}
+	// }
+	getRecords("_dmarc." + domain, "v=DMARC1", &hasDMARC, &dmarcRecord)
+	// dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+   	// if err != nil {
+	// 	log.Printf("Error: %v\n", err)
+   	// }
 
-	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+   	// for _, record := range dmarcRecords {
+	// 	if strings.HasPrefix(record, "v=DMARC1") {
+	// 		hasDMARC = true
+	// 		dmarcRecord = record
+	// 		break
+	// 	}
+	// }
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+}
+
+
+func getRecords(domain string, prefix string, hasEntity *bool, entityRecord *string) {
+	records, err := net.LookupTXT(domain)
    	if err != nil {
 		log.Printf("Error: %v\n", err)
    	}
 
-   	for _, record := range dmarcRecords {
-		if strings.HasPrefix(record, "v=DMARC1") {
-			hasDMARC = true
-			dmarcRecord = record
+   	for _, record := range records {
+		if strings.HasPrefix(record, prefix) {
+			*hasEntity = true
+			*entityRecord = record
 			break
 		}
 	}
-
-	fmt.Printf("%v, %v, %v, %v, %v, %v,", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
 }
